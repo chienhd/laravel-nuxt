1a42383dd29d8626f0843e8f6c6e013539c07e01
Chien Hoang <chienhd@sudobo.com>
2020-07-04 18:26:02 +0700

    reset

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -46,7 +46,7 @@ export default {
   ],
 
   axios: {
-    baseURL: 'http://127.0.0.1:8080/api/auth'
+    baseURL: 'http://127.0.0.1:8000/api/auth'
   },
 
   auth: {
--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,6 +1,5 @@
 export const getters = {
   isAuthenticated(state) {
-  	console.log('state.auth');
   	console.log(state.auth);
     return state.auth.loggedIn
   },
